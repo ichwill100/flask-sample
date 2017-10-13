27e6cf11a47d7ce0331894f9e163a2d957613bc5
ichwill100 <ichwill100@hotmail.com>
2017-10-13 11:38:24 +0200

    Flask running webside in html with a gif inside

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 #!flask-sample/app.py
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
 def hello():
-    return 'Hey, we have Flask in a Docker container! \n Hello World!'
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, use_reloader=True, host='0.0.0.0')
+    #app.run(debug=True, host='0.0.0.0')
     #app.run()
